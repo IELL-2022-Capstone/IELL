69b0fdab07f45931076ca0352a2f6a4fecf60c02
하유진 <hayujin@hayujin-ui-MacBookAir.local>
2022-12-13 06:10:58 +0900

    [Add] InputBox, History

---
+++ components/InputBox.tsx
@@ -0,0 +1,8 @@
+import { Input } from '@chakra-ui/react'
+import React from 'react'
+
+export default function History(){
+    return (
+        <Input placeholder='Basic usage'/>  
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import RadarChart from "../components/RadarChart";
+import InputBox from "../components/InputBox";
+import History from "../components/History";
 
 export default function Home() {
     return (
@@ -9,6 +11,7 @@ export default function Home() {
             <Header />
             <Box minH={800}>
               <Box minH={200} />
+              <InputBox />
               <RadarChart
                 data={[0,1,2,3,4,5]}
                 color={"orange"}
@@ -16,6 +19,7 @@ export default function Home() {
                 height={500}
                 margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
               />
+                <History />
             </Box>
             <Footer />
         </>
