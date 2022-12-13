0ccde05521f2e409a3f5c9317ad3c3cfca0d9d71
nimod7890 <nimod1234@g.skku.edu>
2022-12-14 04:56:30 +0900

    be: cors

--- model/main.py
+++ model/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 import EnglishEssayModel
-from starlette.responses import JSONResponse
 import random
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = {
+    "http://localhost",
+    "http://localhost:3000",
+}
+app.add_middleware(
+   CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials =True,
+    allow_methods = ["*"],
+    allow_headers= ["*"],
+)
 
 
 class Score(BaseModel):
