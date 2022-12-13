3a3e7503958e57e88553ee8e59523c52d3c1b834
nimod7890 <nimod1234@g.skku.edu>
2022-12-14 04:56:49 +0900

    fe: graph 6개 띄우기

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,51 @@ import History from "../components/History";
 import InputBox from "../components/InputBox";
 import { RadarAxis, RadarMark } from "../components/RadarChart";
 
+const WIDTH = 200;
+const HEIGHT = 170;
+const COLOR = [
+  "GoldenRod",
+  "orange",
+  "DarkOrange",
+  "DarkSalmon",
+  "SandyBrown",
+  "LightSalmon"
+];
+const AREA_MARGIN = {
+  top: 20,
+  right: 20,
+  bottom: 40,
+  left: 30
+};
+
+const RADER_MARGIN = {
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0
+};
+
 export default function Home() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<number[]>([]);
   const [history, setHistory] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("http://localhost:3000/api/rader");
-      setData(result.data);
-      const histories = await axios.get("http://localhost:3000/api/history");
-      setHistory(JSON.parse(histories.data));
+      const text = {
+        text: "I think that students would benefit from learning at home,because they wont have to change and get up early in the morning to shower and do there hair. taking only classes helps them because at there house they'll be pay more attention. they will be comfortable at home.The hardest part of school is getting ready. you wake up go brush your teeth and go to your closet and look at your cloths. after you think you picked a outfit u go look in the mirror and youll either not like it or you look and see a stain. Then you'll have to change. with the online classes you can wear anything and stay home and you wont need to stress about what to wear.most students usually take showers before school. they either take it before they sleep or when they wake up. some students do both to smell good. that causes them do miss the bus and effects on there lesson time cause they come late to school. when u have online classes u wont need to miss lessons cause you can get everything set up and go take a shower and when u get out your ready to go.when your home your comfortable and you pay attention. it gives then an advantage to be smarter and even pass there classmates on class work. public schools are difficult even if you try. some teacher dont know how to teach it in then way that students understand it. that causes students to fail and they may repeat the class."
+      };
+      const predict = (await axios.post("http://127.0.0.1:8000/predict", text))
+        .data;
+      const predictData: number[] = [
+        predict.conventions,
+        predict.syntax,
+        predict.vocabulary,
+        predict.phraseology,
+        predict.grammar,
+        predict.conventions
+      ];
+      setData(predictData);
       setIsLoading(false);
     };
     fetchData();
@@ -29,7 +63,7 @@ export default function Home() {
     <>
       <Header />
       {isLoading === false ? (
-        <Box display="grid" gridTemplateAreas={`'i r' 'e h'`}>
+        <Box display="grid" gridTemplateAreas={`'i r' 'e e'`}>
           <Box gridArea="i" minH={200} ml={300}>
             <Box minH={100} />
             <InputBox />
@@ -46,92 +80,140 @@ export default function Home() {
             <Box minH={200} />
             <svg width={300} height={300}>
               <Group top={150} left={150}>
-                <RadarAxis
-                  width={300}
-                  height={300}
-                  margin={{
-                    top: 0,
-                    right: 0,
-                    bottom: 0,
-                    left: 0
-                  }}
-                />
+                <RadarAxis width={300} height={300} margin={RADER_MARGIN} />
                 <RadarMark
-                  data={data[0]}
+                  data={data}
                   color={"orange"}
                   width={300}
                   height={300}
-                  margin={{
-                    top: 0,
-                    right: 0,
-                    bottom: 0,
-                    left: 0
-                  }}
+                  margin={RADER_MARGIN}
                 />
                 <RadarMark
-                  data={data[1]}
+                  data={[3.13, 3.03, 3.24, 3.12, 3.03, 3.08]}
                   color={"blue"}
                   width={300}
                   height={300}
-                  margin={{
-                    top: 0,
-                    right: 0,
-                    bottom: 0,
-                    left: 0
-                  }}
+                  margin={RADER_MARGIN}
                 />
               </Group>
             </svg>
           </Box>
           <Box
-            display="flex"
-            justifyContent="center"
-            alignContent="center"
-            ml={150}
-            mb={10}
+            display="grid"
+            gridTemplateColumns={"repeat(6,220px)"}
             gridArea="e"
+            justifyContent={"center"}
+            alignContent={"center"}
+            overflow="scroll"
           >
-            <svg width={600} height={300}>
-              <AreaAxis
-                width={600}
-                height={300}
-                margin={{
-                  top: 20,
-                  right: 20,
-                  bottom: 30,
-                  left: 30
-                }}
-                data={[0.5, 1, 1.5, 5, 2.5, 3]}
-                color={"orange"}
-              />
-              <AreaMark
-                width={600}
-                height={300}
-                margin={{
-                  top: 20,
-                  right: 20,
-                  bottom: 30,
-                  left: 30
-                }}
-                data={[0.5, 1, 1.5, 5, 2.5, 3]}
-                color={"orange"}
-              />
-              <AreaMark
-                width={600}
-                height={300}
-                margin={{
-                  top: 20,
-                  right: 20,
-                  bottom: 30,
-                  left: 30
-                }}
-                data={[0.5, 1, 1.5, 5, 2.5, 3].reverse()}
-                color={"green"}
-              />
-            </svg>
-          </Box>
-          <Box overflowY="scroll" gridArea="h" w={600} h={300} mt={10} mr={150}>
-            <History {...history} />
+            <Box>
+              <svg width={WIDTH} height={HEIGHT}>
+                <AreaAxis
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[0]}
+                />
+                <AreaMark
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[0]}
+                />
+              </svg>
+            </Box>
+            <Box>
+              <svg width={WIDTH} height={HEIGHT}>
+                <AreaAxis
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[1]}
+                />
+                <AreaMark
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[1]}
+                />
+              </svg>
+            </Box>
+            <Box>
+              <svg width={WIDTH} height={HEIGHT}>
+                <AreaAxis
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[2]}
+                />
+                <AreaMark
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[2]}
+                />
+              </svg>
+            </Box>
+            <Box>
+              <svg width={WIDTH} height={HEIGHT}>
+                <AreaAxis
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[2]}
+                />
+                <AreaMark
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[3]}
+                />
+              </svg>
+            </Box>
+            <Box>
+              <svg width={WIDTH} height={HEIGHT}>
+                <AreaAxis
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[4]}
+                />
+                <AreaMark
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[4]}
+                />
+              </svg>
+            </Box>
+            <Box>
+              <svg width={WIDTH} height={HEIGHT}>
+                <AreaAxis
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[5]}
+                />
+                <AreaMark
+                  width={WIDTH}
+                  height={HEIGHT}
+                  margin={AREA_MARGIN}
+                  data={[0.5, 1, 1.5, 5, 2.5, 3]}
+                  color={COLOR[5]}
+                />
+              </svg>
+            </Box>
           </Box>
         </Box>
       ) : null}
