d1b18e8b914d5791632d809da9f6872d69309c14
하유진 <hayujin@hayujin-ui-MacBookAir.local>
2022-12-14 01:49:57 +0900

    [ADD] commit button

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -1,6 +1,18 @@
-import { Input } from "@chakra-ui/react";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { Button } from '@chakra-ui/react'
 import React from "react";
 
 export default function InputBox() {
-  return <Input placeholder="Basic usage" w={500} h={260} />;
+  return (
+    <InputGroup size='md'>
+      <Input placeholder="Basic usage" w={500} h={260} />
+      <InputRightElement width='4.5rem' display='flex' alignItems='center'>
+        <Button h='1.75rem' size='sm' colorScheme='gray' mt="440" mr="1">Commit</Button>
+      </InputRightElement>
+    </InputGroup>
+    
+    
+
+  )
 }
+
